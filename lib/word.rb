e934d2a56b866c953127cf086c24784581c13b6c
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-30 08:29:03 -0800

    Add spec 1; fails

--- lib/word.rb
+++ lib/word.rb
@@ -1,2 +1,12 @@
 class Word
+  @@word_list = []
+  @@current_id = 0
+
+  # attr_accessor :name :id
+
+  def initialize(name)
+    # @name = name.capitalize
+    # @id = @@current_id
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,3 +1,13 @@
 require('pry')
 require('rspec')
 require('word')
+
+describe('Word') do
+  describe('#initialize') do
+    it('will create a new instance of the Word class') do
+      word = Word.new("intelligent")
+      expect(word.name).to(eq("Intelligent"))
+      expect(word.id).to(eq(0))
+    end
+  end
+end
