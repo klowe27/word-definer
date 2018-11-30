f9705516a281bb909df824e46a59600e284e915c
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-30 08:31:28 -0800

    Pass spec 1; add initialize method to word class

--- lib/word.rb
+++ lib/word.rb
@@ -2,11 +2,11 @@ class Word
   @@word_list = []
   @@current_id = 0
 
-  # attr_accessor :name :id
+  attr_accessor :name, :id
 
   def initialize(name)
-    # @name = name.capitalize
-    # @id = @@current_id
+    @name = name.capitalize
+    @id = @@current_id
   end
 
 end
